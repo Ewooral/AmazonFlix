c6335a2f363484418d24b548e5dd7106f635ce2f
Ewooral <boahen.cos@gmail.com>
2022-02-02 10:23:32 +0000

    working on the homepage of snaps

--- snaps/snaps_frontend/src/components/Login.tsx
+++ snaps/snaps_frontend/src/components/Login.tsx
@@ -41,8 +41,8 @@ const Login = () => {
         />
    
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
-          <div className="p-5">
-            <img src={logo} width="130px" alt="logo" />
+          <div className="p-4">
+            <img src={logo} width="120px" alt="logo" />
           </div>
           <div className="shadow-2xl">
            <GoogleLogin 
